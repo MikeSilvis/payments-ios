2f2511e22749f5a96b8d993eec22876dd14f29ed
Mike Silvis <mikesilvis@gmail.com>
2016-07-25 18:48:42 -0700

    adding digits

--- Payments/AppDelegate.swift
+++ Payments/AppDelegate.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
+import DigitsKit
 
 @UIApplicationMain
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        Fabric.with([Crashlytics.self, Digits.self])
+        
         return true
     }
 
--- Podfile
+++ Podfile
@@ -4,4 +4,8 @@ inhibit_all_warnings!
 target 'Payments' do
   pod 'Masonry'
   pod 'AFNetworking'
+  pod 'Digits'
+  pod 'Fabric'
+  pod 'Crashlytics'
+  pod 'TwitterCore'
 end
